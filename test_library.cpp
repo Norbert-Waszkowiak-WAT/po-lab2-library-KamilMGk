d57854f0902dbfb468f0e92690427c405c32b4d7
KamilMGk <kamillo.michalak@gmail.com>
2025-12-10 22:48:10 +0000

    fdg

--- test_library.cpp
+++ test_library.cpp
@@ -1,48 +1,64 @@
-#ifndef LIBRARY
-#define LIBRARY
-#include <iostream>
-#include "book.cpp"
-using namespace std;
-class Library {
-private:
-Book** books;
-int count;
-public:
-Library(): count(0), books(nullptr){}
-void addBook(Book* book){
-    count++;
-    Book** newBooks = new Book*[count];
-    newBooks[count-1] = book;
-    for(int i = 0; i < count-1; i++){
-        newBooks[i] = books[i];
-    }
-    delete[] books;
-    books = newBooks;
-}
-void removeBook(int index){
-    if(index >= count || index < 0) {
-        cout<<"Invalid index"<<endl;
-        return;
-    }
-    delete books[index];
-    for(int i = index; i < count-1; i ++){
-        books[i] = books [i+1];
-    }
-    count--;
-}
-Book** getBooks(){
-    return books;
-}
-int getCount() {
-    return count;
-}
-~Library(){
-    for (int i = 0; i < count; i++){
-        delete books[i];
-    }
-    delete[] books;
-    cout<<"Library destroyed"<<endl;
-}
-};
-#endif
+#include "catch_amalgamated.hpp"
+#include "library.cpp"
+#include <sstream>
  
+TEST_CASE("Library constructor initializes with zero books", "[constructor]") {
+    Library library;
+    REQUIRE(library.getCount() == 0);
+    REQUIRE(library.getBooks() == nullptr);
+}
+ 
+TEST_CASE("Library addBook method adds a book to the collection", "[addBook]") {
+    Library library;
+    Book* book = new Book("Sample Title", "Sample Author", "123-456-789");
+    library.addBook(book);
+    REQUIRE(library.getCount() == 1);
+    REQUIRE(library.getBooks()[0]->getTitle() == "Sample Title");
+    REQUIRE(library.getBooks()[0]->getAuthor() == "Sample Author");
+    REQUIRE(library.getBooks()[0]->getIsbn() == "123-456-789");
+}
+ 
+TEST_CASE("Library removeBook method removes a book from the collection", "[removeBook]") {
+    Library library;
+    Book* book1 = new Book("Title1", "Author1", "111-111-111");
+    Book* book2 = new Book("Title2", "Author2", "222-222-222");
+    library.addBook(book1);
+    library.addBook(book2);
+    library.removeBook(0);
+    REQUIRE(library.getCount() == 1);
+    REQUIRE(library.getBooks()[0]->getTitle() == "Title2");
+    REQUIRE(library.getBooks()[0]->getAuthor() == "Author2");
+    REQUIRE(library.getBooks()[0]->getIsbn() == "222-222-222");
+}
+ 
+TEST_CASE("Library removeBook method handles invalid index", "[removeBook]") {
+    Library library;
+    Book* book = new Book("Sample Title", "Sample Author", "123-456-789");
+    library.addBook(book);
+    std::ostringstream oss;
+    std::streambuf* p_cout_streambuf = std::cout.rdbuf();
+    std::cout.rdbuf(oss.rdbuf());
+ 
+    library.removeBook(1);
+ 
+    std::cout.rdbuf(p_cout_streambuf);
+    REQUIRE(oss.str() == "Invalid index\n");
+    REQUIRE(library.getCount() == 1);
+}
+ 
+TEST_CASE("Library destructor frees all memory", "[destructor]") {
+    std::ostringstream oss;
+    std::streambuf* p_cout_streambuf = std::cout.rdbuf();
+    std::cout.rdbuf(oss.rdbuf());
+ 
+    {
+        Library library;
+        Book* book1 = new Book("Title1", "Author1", "111-111-111");
+        Book* book2 = new Book("Title2", "Author2", "222-222-222");
+        library.addBook(book1);
+        library.addBook(book2);
+    }
+ 
+    std::cout.rdbuf(p_cout_streambuf);
+    REQUIRE(oss.str() == "Destroying book: Title1\nDestroying book: Title2\nLibrary destroyed\n");
+}
